d375bcc41442a6a4141451435108c5423285fb32
Monica Gil <mgil@psl.com.co>
2017-09-28 10:45:55 -0500

    consumir get

--- test/GithubApi.Gist.test.js
+++ test/GithubApi.Gist.test.js
@@ -21,11 +21,52 @@ describe('Github gist request', () => {
       .auth('token', process.env.ACCESS_TOKEN);
   });
 
-  it('should have guist', () => {
+  it('should have gist', () => {
     guistQuery.then((response) => {
       bodygist = response.body;
       expect(response.status).to.equal(statusCode.CREATED);
       expect(bodygist).to.containSubset(myFirstPromise);
     });
   });
+
+  describe(' get the new gist', () => {
+    let gistQuery;
+
+    before(() => {
+      gistQuery = agent
+        .get(bodygist.url)
+        .auth('token', process.env.ACCESS_TOKEN);
+    });
+
+    it('Gist exists', () =>
+      gistQuery.then(response => expect(response.status).to.equal(statusCode.OK)));
+  });
+
+  describe('delete a gist', () => {
+    let deleteGistQuery;
+
+    before(() => {
+      deleteGistQuery = agent
+        .del(bodygist.url)
+        .auth('token', process.env.ACCESS_TOKEN);
+    });
+
+    it('then the gist should be deleted', () =>
+      deleteGistQuery
+        .then(response => expect(response.status).to.equal(statusCode.NO_CONTENT)));
+  });
+
+   describe('get the delete gist', () => {
+        let notExistGist;
+
+        before(() => {
+          notExistGist = agent
+            .get(gist.url)
+            .auth('token', process.env.ACCESS_TOKEN);
+        });
+
+        it('then the Gits should not be accessible', () =>
+          notExistGist
+            .catch(response => expect(response.status).to.equal(statusCode.NOT_FOUND)));
+      });
 });
